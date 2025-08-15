ad9dae197e0e4ac55960bf61040c3e06e4d90a7a
unknown <ari957752@gmail.com>
2025-08-15 18:17:49 +0700

    p

--- src/components/portfolio/card.jsx
+++ src/components/portfolio/card.jsx
@@ -1,7 +1,12 @@
 import './card.css';
 import { BsFillPeopleFill, BsFillPersonFill, BsFillLaptopFill, BsPhoneFill } from "react-icons/bs";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react';
 
 function CardPort({ title, description, background, buttons = [], icons = [] }) {
+    const [show, setShow] = useState(false);
+
     return (
         <div className="col-lg-6 mb-4">
             <div 
@@ -32,6 +37,9 @@ function CardPort({ title, description, background, buttons = [], icons = [] }) 
                         </div>
                     </div>
                     <div className="button-port d-flex gap-2">
+                        <Button variant="primary" onClick={() => setShow(true)}>
+                            Lihat
+                        </Button>
                         {buttons.map((btn, index) => (
                             <a 
                                 key={index} 
@@ -43,10 +51,38 @@ function CardPort({ title, description, background, buttons = [], icons = [] }) 
                                 {btn.label}
                             </a>
                         ))}
+
+                        
                     </div>
                 </div>
             </div>
+            <Modal
+                show={show}
+                onHide={() => setShow(false)}
+                dialogClassName="modal-90w"
+                aria-labelledby="example-custom-modal-styling-title"
+                data-bs-theme="dark"
+                className='modal'
+            >
+                <Modal.Header closeButton className='bg-dark' border='dark'>
+                    <Modal.Title id="example-custom-modal-styling-title">
+                        Custom Modal Styling
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className='bg-dark'>
+                    <p>
+                        Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
+                        commodi aspernatur enim, consectetur. Cumque deleniti temporibus
+                        ipsam atque a dolores quisquam quisquam adipisci possimus
+                        laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
+                        accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
+                        reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
+                        deleniti rem!
+                    </p>
+                </Modal.Body>
+            </Modal>
         </div>
+        
     );
 }
 
